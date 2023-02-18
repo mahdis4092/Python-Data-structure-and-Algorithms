ac492a63f1bbabba10727ea49a216060d866a0f4
Mahdi hasan Shuvo <mahdishuvo99@gmail.com>
2023-02-19 01:30:15 +0600

    Update sort the matrix in strict order.py

--- Matrix/Basic Operation/sort the matrix in strict order.py
+++ Matrix/Basic Operation/sort the matrix in strict order.py
@@ -11,3 +11,80 @@ Output : 1 2 3
          4 5 6
          7 8 9
 '''
+def sort_matrix(matrix,n):
+    temp=[0]*n*n
+    k=0
+    for i in range(n):
+        for j in range(n):
+            temp[k]=matrix[i][j]
+            k+=1
+
+    temp.sort()
+   # print(temp)
+    k=0
+    for i in range(n):
+        for j in range(n):
+            matrix[i][j]= temp[k]
+            k += 1
+
+
+def printmatrix(matrix,n):
+    for m in range(n):
+        for p in range(n):
+            print(matrix[m][p],end=" ")
+        print("")
+
+
+
+matrix=[[5,4,7],[1,3,8],[2,9,6]]
+n=3
+sort_matrix(matrix,n)
+printmatrix(matrix,n)
+
+# Code with explanation here
+# Python Implementation to sort the given matrix
+def sortMat(mat, n):
+    # temporary matrix of size n^2
+    temp = [0] * n * n
+    k = 0
+
+    # copy the elements of matrix one by one
+    # leto temp[]
+    for i in range(0, n):
+        for j in range(0, n):
+            temp[k] = mat[i][j]
+            k += 1
+
+    # sort temp[]
+    temp.sort(reverse=True)
+
+    # copy the elements of temp[] one by one
+    # in mat[][]
+    k = 0
+    for i in range(0, n):
+        for j in range(0, n):
+            mat[i][j] = temp[k]
+            k += 1
+
+
+# function to print the given matrix
+def printMat(mat, n):
+    for i in range(0, n):
+        for j in range(0, n):
+            print(mat[i][j], end=' ')
+        print("")
+
+
+# Driver program to test above
+mat = [[5, 4, 7],
+       [1, 3, 8],
+       [2, 9, 6]]
+n = 3
+
+print("Original Matrix:")
+printMat(mat, n)
+sortMat(mat, n)
+print("\nMatrix After Sorting:")
+printMat(mat, n)
+
+# This code is contributed by ishankhandelwals.
