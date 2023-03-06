70949525f9f783e5391ff489492ee9bf97cc1119
Mahdi hasan Shuvo <mahdishuvo99@gmail.com>
2023-03-06 22:48:09 +0600

    BST

--- All practice.py
+++ All practice.py
@@ -31,3 +31,62 @@ push(stack,str(40))
 print(pop(stack) + "pop from stack")
 print(peek(stack) + "peek item from stack")
 '''
+'''
+class Node:
+
+    def __init__(self, data, parent):
+        self.data = data
+        self.parent = parent
+        self.right_node = None
+        self.left_node = None
+
+
+class BinarySearchTree:
+
+    def __init__(self):
+        self.root = None
+
+    def insert(self, data):
+        if self.root is None:
+            self.root = Node(data, None)
+        else:
+            self.insert_node(data, self.root)
+
+    def insert_node(self, data, node):
+        # we have to go to the left subtree
+        if data < node.data:
+            if node.left_node:
+                self.insert_node(data, node.left_node)
+            else:
+                node.left_node = Node(data, node)
+        # we have to visit the right subtree
+        else:
+            if node.right_node:
+                self.insert_node(data, node.right_node)
+            else:
+                node.right_node = Node(data, node)
+
+    def traverse(self):
+        if self.root is not None:
+            self.traverse_in_order(self.root)
+
+    def traverse_in_order(self, node):
+
+        if node.left_node:
+            self.traverse_in_order(node.left_node)
+
+        print(node.data)
+
+        if node.right_node:
+            self.traverse_in_order(node.right_node)
+
+
+if __name__ == '__main__':
+
+     bst = BinarySearchTree()
+     bst.insert(5)
+     bst.insert(3)
+     bst.insert(6)
+     bst.insert(1)
+     bst.traverse()
+'''
---
+++ Tree/practice.py
@@ -0,0 +1,79 @@
+class Node:
+    def __init__(self,data,parent=None):
+        self.data=data
+        self.left_node=None
+        self.right_node=None
+        self.parent=parent
+
+class Binarytree:
+
+    def __init__(self):
+        self.root = None
+
+    def insert(self, data):
+        if self.root is None:
+            self.root = Node(data, None)
+        else:
+            self.insert_node(data, self.root)
+
+
+
+
+    def insert_node(self,data,node):
+        if data<node.data:
+            if node.left_node is not None:
+                self.insert_node(data,node.left_node)
+            else:
+                node.left_node=Node(data,Node)
+        else:
+            if node.right_node is not None:
+                self.insert_node(data,node.right_node)
+            else:
+                node.right_node = Node(data, Node)
+
+    def get_min(self):
+        #check node is not null
+        if self.root:
+            return self.get_min_value(self.root)
+    def get_min_value(self,node):
+        if node.left_node:
+            self.get_min_value(node.left_node)
+        return node.data
+
+    def get_max(self):
+        # check node is not null
+        if self.root:
+            return self.get_max_value(self.root)
+
+    def get_max_value(self, node):
+        if node.right_node:
+            self.get_max_value(node.right_node)
+        return node.data
+
+    def traverse(self):
+        self.traverse_in_order(self.root)
+
+    def traverse_in_order(self,node):
+        if node.left_node:
+            self.traverse_in_order(node.left_node)
+        print(node.data)
+
+        if node.right_node:
+            self.traverse_in_order(node.right_node)
+
+
+if __name__ == '__main__':
+    bst=Binarytree()
+    bst.insert(10)
+    bst.insert(-5)
+    bst.insert(20)
+    bst.insert(12)
+    bst.insert(15)
+
+    bst.traverse()
+
+
+
+
+
+
